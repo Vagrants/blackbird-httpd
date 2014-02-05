debd747fbfc5e8483c62d55a9e664c2e5a05db0b
makocchi <makocchi@gmail.com>
2014-02-05 19:15:32 +0900

    fix rpm build setting

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- codig: utf-8 -*-
 
+import os
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='blackbird-httpd',
@@ -12,7 +17,17 @@ setup(
     description=(
         'get httpd stats by using server-status.'
     ),
+    long_description=read('PROJECT.txt'),
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.6',
+    ],
     author='makocchi',
     author_email='makocchi@gmail.com',
     url='https://github.com/Vagrants/blackbird-httpd',
+    data_files=[
+        ('/opt/blackbird/plugins', ['httpd.py']),
+        ('/etc/blackbird/conf.d', ['httpd.cfg'])
+    ],
 )
